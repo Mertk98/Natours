1bddf64e2e6ef2a825dc1f3314c525513fd3d534
Mertk98 <mertkilic27@icloud.com>
2023-01-21 01:27:45 -0800

    added a route for bookings

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -130,6 +131,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookins', bookingRouter);
 
 // If the program is able to reach here, it means that the
 // above router haven't worked. Therefore, program will use this middleware
